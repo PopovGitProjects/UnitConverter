2f1eb08080c79b0d91d7dd8eba5fe38ab3cac250
Popov <popov.workmail@gmail.com>
2021-10-07 00:19:01 +0500

    Первый комит. Приложение крашится при удалени значений из поля ввода.

---
+++ app/src/main/java/com/popov/unitconverter/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.popov.unitconverter
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textViewLeft = findViewById<TextInputEditText>(R.id.editTextLeft)
+        val textViewRight = findViewById<TextInputEditText>(R.id.editTextRight)
+        val tvOutLeft = findViewById<TextView>(R.id.textLeft)
+        val tvOutRight = findViewById<TextView>(R.id.textRight)
+
+        textViewLeft.addTextChangedListener {
+            if (textViewLeft.text != null){
+                val num = textViewLeft.text.toString().toDouble()
+                val result = num * 0.13332239023
+                tvOutLeft.text = result.toString()
+            }
+        }
+
+        textViewRight.addTextChangedListener {
+            if (textViewRight.text != null){
+                val num = textViewRight.text.toString().toDouble()
+                val result = num * 7.5006156
+                tvOutRight.text = result.toString()
+            }else {
+                tvOutRight.text = "0"
+            }
+        }
+    }
+}
