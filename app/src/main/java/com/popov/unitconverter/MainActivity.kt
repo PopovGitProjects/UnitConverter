926b50b37688f8a3058d12323b72f216c390817d
Sergey Popov <popov.workmail@gmail.com>
2021-10-25 17:59:16 +0500

    Добавил bindingData.

--- app/src/main/java/com/popov/unitconverter/MainActivity.kt
+++ app/src/main/java/com/popov/unitconverter/MainActivity.kt
@@ -4,34 +4,45 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
+import com.popov.unitconverter.databinding.ActivityMainBinding
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val textViewLeft = findViewById<TextInputEditText>(R.id.editTextLeft)
         val textViewRight = findViewById<TextInputEditText>(R.id.editTextRight)
 
-        currentFocus
+        val viewLeft = textViewLeft.isTextInputLayoutFocusedRectEnabled
+        val viewRight = textViewRight.isTextInputLayoutFocusedRectEnabled
+
         textViewLeft.addTextChangedListener {
-            if (textViewLeft.text.toString() != ""){
-                val num = textViewLeft.text.toString().toDouble()
-                val result = decimalRound(num * 0.13332239023)
-                textViewRight.setText(result.toString())
-            }else{
-                textViewRight.text = null
+            if (viewLeft){
+                if (textViewLeft.text.toString() != ""){
+                    val num = textViewLeft.text.toString().toDouble()
+                    val result = decimalRound(num * 0.13332239023)
+                    textViewRight.setText(result.toString())
+                }else{
+                    textViewRight.text = null
+                }
+
             }
         }
 
         textViewRight.addTextChangedListener {
-            if (textViewRight.text.toString() != ""){
-                val num = textViewRight.text.toString().toDouble()
-                val result = decimalRound(num * 7.5006156)
-                textViewLeft.setText(result.toString())
-            }else{
-                textViewLeft.text = null
+            if (viewRight){
+                if (textViewRight.text.toString() != ""){
+                    val num = textViewRight.text.toString().toDouble()
+                    val result = decimalRound(num * 7.5006156)
+                    textViewLeft.setText(result.toString())
+                }else{
+                    textViewLeft.text = null
+                }
+
             }
         }
     }
